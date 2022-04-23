1f666dc12a2bf4396332f429a4efcf29b54fe6f6
Palash Mandal <pmpalashmandal@hmail.com>
2022-04-23 22:45:16 +0600

    map item and ScrollView on ui

--- App.js
+++ App.js
@@ -1,29 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+} from 'react-native';
 import { useState } from 'react';
 
 export default function App() {
-  const [name, setName] = useState('Palash');
-  const [age, setAge] = useState(24);
+  const [fruit, setFruit] = useState([
+    { name: 'Apple', key: 1 },
+    { name: 'Banana', key: 2 },
+    { name: 'Mango', key: 3 },
+    { name: 'Orange', key: 4 },
+    { name: 'Apple', key: 5 },
+    { name: 'Mango', key: 6 },
+    { name: 'Orange', key: 7 },
+    { name: 'Apple', key: 8 },
+    { name: 'Orange', key: 9 },
+    { name: 'Apple', key: 10 },
+  ]);
 
   return (
     <View style={styles.container}>
-      <Text>Enter your name: </Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={(text) => setName(text)}
-        placeholder="Enter name here.."
-      />
-      <Text>Enter your age: </Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={(text) => setAge(text)}
-        placeholder="Enter age here.."
-        keyboardType="numeric"
-      />
-      <Text>
-        My name is {name} and age is {age} years old.
-      </Text>
+      <ScrollView>
+        {fruit.map((item) => {
+          return (
+            <View key={item.key}>
+              <Text style={styles.itemStyle}>{item.name}</Text>
+            </View>
+          );
+        })}
+      </ScrollView>
     </View>
   );
 }
@@ -32,14 +42,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 40,
   },
-  input: {
-    borderColor: 'black',
-    borderWidth: 1,
-    padding: 10,
-    margin: 10,
-    width: 150,
+  itemStyle: {
+    fontSize: 22,
+    backgroundColor: 'yellow',
+    color: 'black',
+    marginTop: 20,
+    padding: 20,
   },
 });
