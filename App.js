74fc480a1a198d1ab243cc53a1fbfb1fc1cea38d
Palash Mandal <pmpalashmandal@hmail.com>
2022-04-24 17:56:44 +0600

    Modal screen

--- App.js
+++ App.js
@@ -12,21 +12,38 @@ import {
   Keyboard,
   Alert,
   Image,
+  Modal,
 } from 'react-native';
 import { useState, useEffect } from 'react';
 import styles from './style';
 
 export default function App() {
+  const [modal, setModal] = useState(false);
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(0);
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ backgroundColor: 'red', flex: 1 }}></View>
-      <View style={{ backgroundColor: 'green', flex: 1 }}></View>
-      <View style={{ backgroundColor: 'blue', flex: 2 }}></View>
-      <View style={{ backgroundColor: 'yellow', flex: 1 }}></View>
-      <View style={{ backgroundColor: 'black', flex: 1 }}></View>
-      <View style={{ backgroundColor: 'green', flex: 1 }}></View>
-      <View style={{ backgroundColor: 'blue', flex: 1 }}></View>
-      <View style={{ backgroundColor: 'yellow', flex: 1 }}></View>
+    <View style={styles.container}>
+      <Text>Main screen____</Text>
+      <TextInput
+        style={styles.inputStyle}
+        onChangeText={(text) => setName(text)}
+        placeholder="name"
+      />
+      <TextInput
+        style={styles.inputStyle}
+        onChangeText={(text) => setAge(text)}
+        placeholder="Age"
+      />
+      <Button title="Open Modal" onPress={() => setModal(true)} />
+      <Modal visible={modal}>
+        <View style={styles.container}>
+          <Text>_____Modal screen</Text>
+          <Text>
+            Name: {name} , Age: {age}
+          </Text>
+          <Button title="Close Modal" onPress={() => setModal(false)} />
+        </View>
+      </Modal>
     </View>
   );
 }
