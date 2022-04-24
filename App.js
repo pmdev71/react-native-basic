84e979fac54917958899ac1971d90f00e8cc4f66
Palash Mandal <pmpalashmandal@hmail.com>
2022-04-24 18:58:45 +0600

    import component from different path

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import {
   StyleSheet,
   Text,
@@ -16,34 +15,14 @@ import {
 } from 'react-native';
 import { useState, useEffect } from 'react';
 import styles from './style';
+import Greeting from './Greeting';
+import InputCom from './components/inputCom';
 
 export default function App() {
-  const [modal, setModal] = useState(false);
-  const [name, setName] = useState('');
-  const [age, setAge] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Main screen____</Text>
-      <TextInput
-        style={styles.inputStyle}
-        onChangeText={(text) => setName(text)}
-        placeholder="name"
-      />
-      <TextInput
-        style={styles.inputStyle}
-        onChangeText={(text) => setAge(text)}
-        placeholder="Age"
-      />
-      <Button title="Open Modal" onPress={() => setModal(true)} />
-      <Modal visible={modal}>
-        <View style={styles.container}>
-          <Text>_____Modal screen</Text>
-          <Text>
-            Name: {name} , Age: {age}
-          </Text>
-          <Button title="Close Modal" onPress={() => setModal(false)} />
-        </View>
-      </Modal>
+      <Greeting />
+      <InputCom />
     </View>
   );
 }
---
+++ components/inputCom.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+  FlatList,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Alert,
+  Image,
+  Modal,
+} from 'react-native';
+import styles from '../style';
+
+export default function InputCom() {
+  const [name, setName] = useState('');
+
+  return (
+    <View style={styles.container}>
+      <Text>From inputCom.js Component</Text>
+      <TextInput
+        style={styles.inputStyle}
+        onChangeText={(text) => setName(text)}
+        placeholder="name"
+      />
+
+      <Button title="Click" onPress={() => Alert.alert(name)} />
+    </View>
+  );
+}
