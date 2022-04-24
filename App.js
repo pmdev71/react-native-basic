22cf47959df3dca4fbc6f8a4725dec0cb63cfa76
Palash Mandal <pmpalashmandal@hmail.com>
2022-04-24 21:04:54 +0600

    multiple switch button

--- App.js
+++ App.js
@@ -18,20 +18,45 @@ import { useState, useEffect } from 'react';
 import styles from './style';
 
 export default function App() {
-  const [isEnable, setIsEnable] = useState(false);
+  const [location, setLocation] = useState(false);
+  const [storage, setStorage] = useState(false);
+  const [mic, setMic] = useState(false);
 
-  const toggleSwitch = () => {
-    setIsEnable((previousState) => !previousState);
-    Alert.alert('Value changed.');
+  const toggleLocation = () => {
+    setLocation((previousState) => !previousState);
+    Alert.alert('Value changed!');
+  };
+  const toggleStorage = () => {
+    setStorage((previousState) => !previousState);
+    Alert.alert('Value changed!');
+  };
+  const toggleMic = () => {
+    setMic((previousState) => !previousState);
+    Alert.alert('Value changed!');
   };
 
   return (
     <View style={styles.container}>
+      <Text>Location access {location ? 'On' : 'Off'}</Text>
       <Switch
-        value={isEnable}
+        value={location}
         trackColor={{ false: 'red', true: 'green' }}
-        thumbColor={isEnable ? 'yellow' : 'pink'}
-        onValueChange={toggleSwitch}
+        thumbColor={location ? 'yellow' : 'pink'}
+        onValueChange={toggleLocation}
+      />
+      <Text>Storage access {storage ? 'On' : 'Off'}</Text>
+      <Switch
+        value={storage}
+        trackColor={{ false: 'red', true: 'green' }}
+        thumbColor={storage ? 'yellow' : 'pink'}
+        onValueChange={toggleStorage}
+      />
+      <Text>Mic access {mic ? 'On' : 'Off'}</Text>
+      <Switch
+        value={mic}
+        trackColor={{ false: 'red', true: 'green' }}
+        thumbColor={mic ? 'yellow' : 'pink'}
+        onValueChange={toggleMic}
       />
     </View>
   );
