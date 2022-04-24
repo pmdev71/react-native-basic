05099ba890bfbbe5c7b6576299cf2da49d4d20bf
Palash Mandal <pmpalashmandal@hmail.com>
2022-04-24 14:59:29 +0600

    TouchableWithoutFeedback and Keybord for hide keyboard

--- App.js
+++ App.js
@@ -8,6 +8,8 @@ import {
   ScrollView,
   FlatList,
   TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
   Alert,
   Image,
 } from 'react-native';
@@ -16,43 +18,16 @@ import styles from './style';
 
 export default function App() {
   const [name, setName] = useState('');
-  const [age, setAge] = useState(0);
-  const [gender, setGender] = useState('');
-
-  const pressHandler = () => {
-    Alert.alert('Details', 'Are you sure you submit this information?', [
-      { text: 'Yes', onPress: () => Alert.alert('Thanks') },
-      { text: 'No', onPress: () => resetHandler() },
-    ]);
-  };
-
-  const resetHandler = () => {
-    setName('');
-    setAge(0);
-    setGender('');
-  };
 
   return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.inputStyle}
-        onChangeText={(text) => setName(text)}
-        placeholder="Name"
-      />
-      <TextInput
-        style={styles.inputStyle}
-        onChangeText={(text) => setAge(text)}
-        placeholder="Age"
-      />
-      <TextInput
-        style={styles.inputStyle}
-        onChangeText={(text) => setGender(text)}
-        placeholder="Gender"
-      />
-      <Button title="Submit" onPress={() => pressHandler()} />
-      <Text>
-        {'Name: ' + name}, {'Age: ' + age}, {'Gender: ' + gender}
-      </Text>
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.inputStyle}
+          onChangeText={(text) => setName(text)}
+          placeholder="Name"
+        />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
