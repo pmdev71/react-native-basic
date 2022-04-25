a7f455c39ba4bb5392367bd5a44c73bb0d19dfa2
Palash Mandal <pmpalashmandal@hmail.com>
2022-04-25 18:15:39 +0600

    tost message using TostAndroid-3types

--- App.js
+++ App.js
@@ -17,28 +17,45 @@ import {
   SectionList,
   Pressable,
   RefreshControl,
+  ToastAndroid,
 } from 'react-native';
 import { useState, useEffect } from 'react';
 import styles from './style';
 
 export default function App() {
-  const [refresh, setRefresh] = useState(false);
+  const showTost = () => {
+    ToastAndroid.show('Welcome to Android', ToastAndroid.SHORT);
+  };
 
-  const pullMe = () => {
-    setRefresh(true);
-    setTimeout(() => {
-      setRefresh(false);
-    }, 2000);
+  const showWithGravity = () => {
+    ToastAndroid.showWithGravity(
+      'Welcome to Android',
+      ToastAndroid.SHORT,
+      ToastAndroid.CENTER
+    );
+  };
+
+  const showWithGravityAndOffset = () => {
+    ToastAndroid.showWithGravityAndOffset(
+      'Welcome to Android showWithGravityAndOffset',
+      ToastAndroid.SHORT,
+      ToastAndroid.CENTER,
+      90,
+      100
+    );
   };
 
   return (
     <View style={styles.container}>
-      <Text>Pull for Refresh</Text>
-      <ScrollView
-        refreshControl={
-          <RefreshControl refreshing={refresh} onRefresh={() => pullMe()} />
-        }
-      />
+      <Button title="showTost" onPress={() => showTost()}></Button>
+      <Button
+        title="showTostWithGravity"
+        onPress={() => showWithGravity()}
+      ></Button>
+      <Button
+        title="showTostWithGravityAndOffset"
+        onPress={() => showWithGravityAndOffset()}
+      ></Button>
     </View>
   );
 }
