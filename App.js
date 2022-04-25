a2c2a39be26b58d23ea93cc5ce766d18b08928f5
Palash Mandal <pmpalashmandal@hmail.com>
2022-04-25 16:30:58 +0600

    RefreshControl for reload page interface

--- App.js
+++ App.js
@@ -16,31 +16,29 @@ import {
   ActivityIndicator,
   SectionList,
   Pressable,
+  RefreshControl,
 } from 'react-native';
 import { useState, useEffect } from 'react';
 import styles from './style';
 
 export default function App() {
-  const [count1, setCount1] = useState(0);
-  const [count2, setCount2] = useState(0);
+  const [refresh, setRefresh] = useState(false);
+
+  const pullMe = () => {
+    setRefresh(true);
+    setTimeout(() => {
+      setRefresh(false);
+    }, 2000);
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Click Count: {count1}</Text>
-      <Text>Press Count: {count2}</Text>
-      <Pressable>
-        <Text onPress={() => setCount1(count1 + 1)}>Click me</Text>
-        <Button title="Press" onPress={() => setCount2(count2 + 1)}></Button>
-      </Pressable>
-      <Pressable
-        style={({ pressed }) => [
-          { backgroundColor: pressed ? 'green' : 'red' },
-        ]}
-      >
-        {({ pressed }) => (
-          <Text> {pressed ? 'Click Plz' : 'Thanks for Click'}</Text>
-        )}
-      </Pressable>
+      <Text>Pull for Refresh</Text>
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={refresh} onRefresh={() => pullMe()} />
+        }
+      />
     </View>
   );
 }
