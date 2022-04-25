fe90d6949aa870fb929b43222fb9024f8e605cd4
Palash Mandal <pmpalashmandal@hmail.com>
2022-04-25 19:08:07 +0600

    device information using Platform

--- App.js
+++ App.js
@@ -19,54 +19,18 @@ import {
   RefreshControl,
   ToastAndroid,
   Appearance,
+  Platform,
 } from 'react-native';
 import { useState, useEffect } from 'react';
 import styles from './style';
 
 export default function App() {
-  // get light/dark based on device mood
-  const colorScheme = Appearance.getColorScheme();
-
-  const [name, setName] = useState('');
-  const [age, setAge] = useState(0);
-
   return (
-    <View
-      style={{
-        flex: 1,
-        padding: 20,
-        backgroundColor: colorScheme === 'light' ? 'white' : 'gray',
-      }}
-    >
-      <TextInput
-        style={{
-          marginTop: 20,
-          padding: 15,
-          backgroundColor: colorScheme === 'light' ? 'gray' : 'white',
-          color: colorScheme === 'light' ? 'white' : 'black',
-        }}
-        onChangeText={(text) => setName(text)}
-        placeholder="Name..."
-      />
-      <TextInput
-        style={{
-          marginTop: 20,
-          padding: 15,
-          backgroundColor: colorScheme === 'light' ? 'gray' : 'white',
-          color: colorScheme === 'light' ? 'white' : 'black',
-        }}
-        onChangeText={(text) => setAge(text)}
-        placeholder="Age..."
-      />
-
-      <Text
-        style={{
-          fontSize: 20,
-          color: colorScheme === 'light' ? 'blue' : 'white',
-        }}
-      >
-        Name: {name}, Age: {age}
-      </Text>
+    <View style={styles.container}>
+      <Text>OS : {Platform.OS}</Text>
+      <Text>OS Version : {Platform.Version}</Text>
+      <Text>isTV : {Platform.isTV.toString()}</Text>
+      <Text>Constants : {JSON.stringify(Platform.constants, null, 2)}</Text>
     </View>
   );
 }
